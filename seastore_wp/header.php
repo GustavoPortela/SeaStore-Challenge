f59a0e602d0e1e33b7b62bcf459bd0b398ca2605
GustavoPortela <36083762+GustavoPortela@users.noreply.github.com>
2020-05-03 17:21:26 -0300

    Implementação WordPress

---
+++ seastore_wp/header.php
@@ -0,0 +1,26 @@
+<!doctype HTML>
+<html <?php language_attributes(); ?>>
+<html>
+
+
+
+<head>
+    <!--Google icons-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/369dbcc70f.js" crossorigin="anonymous"></script>
+
+    <!-- Google Font --> 
+    <link href="https://fonts.googleapis.com/css?display=swap&family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet">
+
+
+
+    <?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    
+
+    </head>
+
+<body <?php body_class(); ?>
